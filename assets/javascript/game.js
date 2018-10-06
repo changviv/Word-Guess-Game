7da3cea92f4fea352e4257391214c117ae19852c
Vivien <vchang103@gmail.com>
2018-10-05 22:28:03 -0700

    edit game script. Add beginning css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,43 @@
-
-// Array list of shows
 var nickShows = ["catdog", "rockos", "allthat", "rugrats", "heyarnold", "angrybeavers", "renandstimpy", "doug", "spongebob", "rocketpower"]
 
-// generates one random show
 var show;
-var wins = 0
+var wins = 0;
 var remainingLetters;
-var guessLeft = 10
+var guessLeft = 10;
 var guessedWord = [];
 var guessedLetters = [];
 
-
 function guessesLeftFunction() {
-  return document.getElementById("guesses-left").innerHTML = guessLeft
+  return document.getElementById("guesses-left").innerHTML = guessLeft;
 }
 
 function winFunction() {
-	return document.getElementById("wins").innerHTML = wins
+	return document.getElementById("wins").innerHTML = wins;
 }
-
-function startGame() {
-	guessesLeftFunction();
-	winFunction()
-	show = nickShows[Math.floor(Math.random() * nickShows.length)];
-	console.log("the show name: " + show)
-	remainingLetters = show.length
-	guessedWord = [];
-	for (var i=0; i < show.length; i++) {
-		guessedWord.push("_")
-	}
-	return document.getElementById("word").innerHTML = guessedWord
-}
-
-function displayScore() {
-	document.getElementById("letters").innerHTML = ""
-	guessLeft = 10;
-	wins;
-	winFunction();
-	guessesLeftFunction();
-};
-
 
 function guessedLetters() {
 	return document.getElementById("letters").innerHTML = "";
 }
 
+function startGame() {
+	show = nickShows[Math.floor(Math.random() * nickShows.length)];
+	guessesLeftFunction();
+	winFunction();
+	remainingLetters = show.length;
+	guessedWord = [];
+	for (var i=0; i < show.length; i++) {
+		guessedWord.push("_");
+	}
+	document.getElementById("word").innerHTML = guessedWord;
+}
 
+function displayScore() {
+	document.getElementById("letters").innerHTML = "";
+	guessLeft = 10;
+	wins;
+	winFunction();
+	guessesLeftFunction();
+};
 
 // initialize game
 startGame();
@@ -54,7 +46,6 @@ displayScore();
 document.onkeyup = function(event) {
 	var userChoice = event.key.toLowerCase()
 	var wordIndex = [];
-	var new_index;
 
 	for (var i=0; i < show.length; i++) {
 		if (userChoice === show[i]) {
@@ -64,15 +55,14 @@ document.onkeyup = function(event) {
 
 	if (wordIndex.length > 0) {
 		for (var j=0; j < wordIndex.length; j++) {
-			new_index = wordIndex[j]
-			guessedWord[new_index] = userChoice
+			guessedWord[wordIndex[j]] = userChoice
 		}
 		document.getElementById("word").innerHTML = guessedWord
 		remainingLetters -= wordIndex.length
 	} else {
 		guessLeft--
-		guessesLeftFunction()
-		document.getElementById("letters").innerHTML += (userChoice + " ")
+		guessesLeftFunction();
+		document.getElementById("letters").innerHTML += (userChoice + " ");
 	};
 
 	wordIndex = [];
@@ -80,13 +70,13 @@ document.onkeyup = function(event) {
 	if ((remainingLetters === 0) || (guessLeft === 0)) {
 		if (remainingLetters === 0) {
 			wins++;
-			displayScore()
-			startGame()
+			displayScore();
+			startGame();
 		} else {
 			displayScore();
 			startGame();
-		};
-	}
+		}
+	};
 
 };
 
