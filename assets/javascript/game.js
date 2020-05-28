038fd976b4c9cc8c899ac5450ec1de76ff348052
Vivien <vchang103@gmail.com>
2020-05-27 18:38:32 -0700

    add pop modal, fix to only allow letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -38,42 +38,69 @@ function displayScore() {
 	guessesLeftFunction();
 };
 
+function lettersOnly(userChoice) {
+  if (userChoice.slice(0,3) === "Key") {
+  	return true
+  }
+  return false
+}
+
+function displayModal(bool) {
+	if (bool === true) {
+		var text = "Congrats! You're a Nick Kid! Want to play again?";
+		$(".modal-font").text(text);
+	} else {
+		var text = "Oh no! Better luck next time!";
+		$(".modal-font").text(text);
+	}
+
+	$('#myModal').modal('show');
+};
+
+$(".playAgain").click(function() {
+	$('#myModal').modal('hide');
+	displayScore();
+	startGame();
+});
+
 // initialize game
 startGame();
 displayScore();
 
 document.onkeyup = function(event) {
-	var userChoice = event.key.toLowerCase()
+	var userInput = event.code;
 	var wordIndex = [];
-
-	for (var i=0; i < show.length; i++) {
-		if (userChoice === show[i]) {
-			wordIndex.push(i);
+	
+	var checkLetter = lettersOnly(userInput);
+	if (checkLetter == true) {
+		var userChoice = event.key.toLowerCase();
+		for (var i=0; i < show.length; i++) {
+			if (userChoice === show[i]) {
+				wordIndex.push(i);
+			}
 		}
-	}
 
-	if (wordIndex.length > 0) {
-		for (var j=0; j < wordIndex.length; j++) {
-			guessedWord[wordIndex[j]] = userChoice
-		}
-		document.getElementById("word").innerHTML = guessedWord.join('  ')
-		remainingLetters -= wordIndex.length
-	} else {
-		guessLeft--
-		guessesLeftFunction();
-		document.getElementById("letters").innerHTML += (userChoice + " ");
-	};
-
-	wordIndex = [];
-
-	if ((remainingLetters === 0) || (guessLeft === 0)) {
-		if (remainingLetters === 0) {
-			wins++;
-			displayScore();
-			startGame();
+		if (wordIndex.length > 0) {
+			for (var j=0; j < wordIndex.length; j++) {
+				guessedWord[wordIndex[j]] = userChoice;
+			}
+			document.getElementById("word").innerHTML = guessedWord.join('  ');
+			remainingLetters -= wordIndex.length;
 		} else {
-			displayScore();
-			startGame();
+			guessLeft--;
+			guessesLeftFunction();
+			document.getElementById("letters").innerHTML += (userChoice + " ");
 		}
-	};
+
+		wordIndex = [];
+
+		if ((remainingLetters === 0) || (guessLeft === 0)) {
+			if (remainingLetters === 0) {
+				wins++;
+				displayModal(true);
+			} else {
+				displayModal(false);
+			}
+		}
+	} 
 };
